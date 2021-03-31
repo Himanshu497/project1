47737b14c51dee5c6046da285d148fe4d8ecd4fc
git1 <himanshurajput4497@gmail.com>
2021-03-31 18:10:28 +0530

    first commit

---
+++ app2/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+app_name='app2'
+from app import views
+urlpatterns=[
+    path('him/',views.hai,name='him'),
+    path('singh/',views.hello,name='singh'),
+    path('singh1/',views.htmlfile,name='singh1'),
+]
---
+++ app2/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+
+from django.http import HttpResponse
+
+def htmlfile(request):
+    return render(request,'app2\coffie.html')
+
+def hai(request):
+    return HttpResponse('<h1><style="color:blue"<marquee>HI HIMANSHU THIS IS FIRST DJANGO PROJECT</h1></marquee>')
+
+def hello(request):
+    return HttpResponse('<h2>THIS IS SECOND FUNCTION IN VIEWS FILE CALLED SEPERTAELY</h2>')
+
+
+    
+
